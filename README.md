a955896dedfded39c0e383bfea770d41cac32b6f
Fabrício de Figueiredo Miranda <125789205+fabriciofianda@users.noreply.github.com>
2023-02-23 03:10:05 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# CalculoDistanciaMedia
 Programa gerado em ChatGPT na linguagem python para cálculo da distância média da Terra em relação ao Sol ao Longo do tempo utilizando a Relatividade geral
---
+++ calculo_distancia.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define as constantes
+G = 6.67430e-11  # constante gravitacional em m^3/kg s^2
+c = 299792458    # velocidade da luz em m/s
+M = 1.989e30     # massa do Sol em kg
+
+# Define a função para calcular a distância Terra-Sol
+def distancia_tempo(t):
+    # Define as condições iniciais
+    x0 = 147e9         # distância média Terra-Sol em m
+    v0 = 0             # velocidade inicial em m/s
+    r0 = np.array([x0, 0, 0])  # posição inicial em m
+    v0 = np.array([0, v0, 0])  # velocidade inicial em m/s
+
+    # Define o passo de tempo e o número de iterações
+    dt = 60*60*24       # passo de um dia em segundos
+    N = int(t/dt) + 1   # número de iterações
+
+    # Define os vetores de posição e velocidade
+    r = np.zeros([N, 3])
+    v = np.zeros([N, 3])
+    r[0] = r0
+    v[0] = v0
+
+    # Itera para calcular a posição e velocidade em cada passo de tempo
+    for i in range(1, N):
+        # Calcula a aceleração gravitacional usando a Relatividade Geral
+        r_norm = np.linalg.norm(r[i-1])
+        a = -G*M/(r_norm**3) * (1 + (3*G*M)/(r_norm*c**2))
+
+        # Calcula a nova posição e velocidade usando o método de Verlet
+        r[i] = r[i-1] + v[i-1]*dt + 0.5*a*r[i-1]*(dt**2)
+        r_norm = np.linalg.norm(r[i])
+        a_new = -G*M/(r_norm**3) * (1 + (3*G*M)/(r_norm*c**2))
+        v[i] = v[i-1] + 0.5*(a_new + a)*dt
+
+    # Retorna a distância média Terra-Sol em metros
+    return np.mean(np.linalg.norm(r, axis=1))
+
+# Define o período de tempo a ser calculado
+t_min = 0       # tempo mínimo em segundos
+t_max = 31536000 # tempo máximo em segundos (um ano)
+
+# Cria um vetor de tempos igualmente espaçados
+t = np.linspace(t_min, t_max, 100)
+
+# Calcula a distância média Terra-Sol em metros para cada tempo
+d = np.zeros(len(t))
+for i in range(len(t)):
+    d[i] = distancia_tempo(t[i])
+
+# Plota o gráfico da distância média Terra-Sol em função do tempo
+plt.plot(t/(60*60*24*365), d/1e9)
+plt.xlabel('Tempo (anos)')
+plt.ylabel('Distância média Terra-Sol (Gm)')
+plt.show()
